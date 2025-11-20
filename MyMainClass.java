4c256a7a3fed7fca38333ae41da69a58a0aa3220
chandankumar HM <chandu252820@gmail.com>
2025-11-20 12:04:10 +0530

    Update MyMainClass.java

--- MyMainClass.java
+++ MyMainClass.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class MyMainClass {
     public static void main(String[] args) {
-        System.out.println("Hello from a Java app!");
+        System.out.println("Hello from an Java app!");
     }
 }
